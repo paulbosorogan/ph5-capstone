6984d358eb0e74ece1a0642eb84aadbb442aae00
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-11-27 19:05:29 -0500

    Created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '/me', to: 'users#show'
+  post '/signup', to: 'users#create'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/order-items', to: 'order_items#index'
+  resources :items, only: [:index, :create, :update, :destroy]
+  resources :orders, only: [:index, :create]
+  
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+    
 end
