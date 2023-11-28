0d672d9a77492b1dd289f62fadcbdcdc32610f1e
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-11-27 19:10:59 -0500

    Added components and set up App

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { UserProvider } from './contexts/UserContext';
+import {BrowserRouter} from 'react-router-dom'
+import { ItemsProvider } from './contexts/ItemsContext';
+import { OrdersProvider } from './contexts/OrdersContext';
+import { ReactNotifications } from 'react-notifications-component';
+import 'react-notifications-component/dist/theme.css'
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+      <UserProvider>
+        <OrdersProvider>
+        <ItemsProvider>
+          <ReactNotifications/>
+          <App />
+      </ItemsProvider>
+      </OrdersProvider>
+    </UserProvider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
