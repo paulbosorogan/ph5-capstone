426bacedcd8550d213b5ebafa26fd3b2d092e032
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-11-28 17:25:11 -0500

    Updated typo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address:         'smtp.gmail.com',
   port:            587,
-  user_name:       EMV["EMAIL"],
+  user_name:       ENV["EMAIL"],
   password:        ENV["EMAILPASSWORD"],
   authentication:  'plain',
   enable_starttls_auto: true  }
