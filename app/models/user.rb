38ec50641b3ee6bf7ecead3d7db255d79ce44765
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-11-29 12:40:50 -0500

    Updated validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,15 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :name, :email, :address, :phone,  presence: true 
-    validates :email, uniqueness: true
+    validates :email, :phone, uniqueness: true
+    validates :my_email_attribute, email: {mode: strict, require_fqdn: true}
+
+    validates :phone, phone: true, allow_blank: false, uniqueness: true
+    before_save :format_phone
 
     has_many :orders, dependent: :destroy
+
+    def format_phone 
+        self.phone = Phonelib.parse(phone).e164
+    end
 end
