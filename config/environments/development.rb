3b3a48e006730dd6931a1cc04b8ee8f69ca037ea
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-11-29 21:41:32 -0500

    Updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   address:         'smtp.gmail.com',
   port:            587,
   user_name:       ENV["EMAIL"],
-  password:        ENV["EMAILPASSWORD"],
+  password:        ENV["PASSWORD"],
   authentication:  'plain',
   enable_starttls_auto: true  }
   
